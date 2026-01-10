8374236fb09796fcbfb9c74fbd6b4058ce93d8ad
ccirelli2 <ccirelli2@gmail.com>
2026-01-10 11:37:40 -0500

    misc

--- courses/quick-start/weather-agent/agent.py
+++ courses/quick-start/weather-agent/agent.py
@@ -5,29 +5,34 @@ url: https://docs.langchain.com/oss/python/langchain/quickstart
 import os
 from langchain.agents.factory import create_agent
 from dotenv import load_dotenv
+from pprint import pprint
 load_dotenv()
 
 MODEL = "gpt-5-nano"
+
 
 # Tools
 def get_weather(city: str) -> str:
     """Get weather for a given city"""
     return f"It's always sunny in {city}"
 
+
 def add_two_numbers(num1: int, num2: int) -> int:
     """Add two numbers"""
     return num1 + num2
+
 
 # Agent
 agent = create_agent(
     model=MODEL,
     tools=[get_weather, add_two_numbers],
-    system_prompt="You are an AI assistant",
+    system_prompt="You are an AI assistant.  You must use a tool for all arithmetic questions before providing a response.",
 )
 
 # Execute the Agent
-user_question = ("What is two plus two?")
-response = agent.invoke({"messages": [("user", user_question)]})
-messages = response['messages']
-for i in messages:
-    print(i)
+user_question = "What is two plus two?"
+response = agent.invoke(
+    {"messages": [("user", user_question)]}, config={"tool_choice": "required"}
+)
+for msg in response['messages']:
+    msg.pretty_print()
