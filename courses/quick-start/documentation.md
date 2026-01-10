cd61a97e9cebdce6c3cde7265d2fbd1fa6fc7925
ccirelli2 <ccirelli2@gmail.com>
2026-01-10 08:42:42 -0500

    update env.example file. added a new tool to agent.py

--- courses/quick-start/documentation.md
+++ courses/quick-start/documentation.md
@@ -8,3 +8,6 @@
 
 ### Weather Agent
 - url: https://docs.langchain.com/oss/python/langchain/quickstart
+
+### Setting Up LangSmith Tracing
+- https://smith.langchain.com/o/3ff1e4f2-393c-54fd-9a91-177e90642137?paginationModel=%7B%22pageIndex%22%3A0%2C%22pageSize%22%3A5%7D
--- courses/quick-start/weather-agent/agent.py
+++ courses/quick-start/weather-agent/agent.py
@@ -9,21 +9,25 @@ load_dotenv()
 
 MODEL = "gpt-5-nano"
 
-# Tool
+# Tools
 def get_weather(city: str) -> str:
     """Get weather for a given city"""
     return f"It's always sunny in {city}"
 
+def add_two_numbers(num1: int, num2: int) -> int:
+    """Add two numbers"""
+    return num1 + num2
 
 # Agent
 agent = create_agent(
     model=MODEL,
-    tools=[get_weather],
+    tools=[get_weather, add_two_numbers],
     system_prompt="You are an AI assistant",
 )
 
 # Execute the Agent
-user_question = "What is the weather like in Atlanta"
+user_question = ("What is two plus two?")
 response = agent.invoke({"messages": [("user", user_question)]})
-print("User question => {}".format(user_question))
-
+messages = response['messages']
+for i in messages:
+    print(i)
